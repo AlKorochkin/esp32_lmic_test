87353d0316750f1427bb0eac6006c71c8f1dde00
alko <ka1ll1cit@gmail.com>
2023-06-29 20:17:43 +0500

    add secrets example

---
+++ esp_secrets.example.h
@@ -0,0 +1,7 @@
+// Rename this file to "esp_secrets.h" and past your values
+#define APPEUI "SET APPEUI"
+#define DEVEUI "SET DEVEUI"
+#define DEVKEY "SET DEVKEY"
+#define DEVADDR "SET DEVADDR"
+#define DEVICEID "SET DEVICEID"
+#define ARTKEY "SET ARTKEY"
